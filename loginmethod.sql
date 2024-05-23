77eb3cbb8882e8c6808cfb5d56027c6fa5a4648f
Owxyjem <jeremiahlansang1010@gmail.com>
2024-05-23 15:46:22 +0800

    new db

--- loginmethod.sql
+++ loginmethod.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2024 at 02:13 AM
+-- Generation Time: May 23, 2024 at 09:45 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -34,24 +34,24 @@ CREATE TABLE `users` (
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `birthday` date DEFAULT NULL,
-  `sex` varchar(255) NOT NULL
+  `sex` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_profile_picture` varchar(255) NOT NULL,
+  `account_type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`User_Id`, `username`, `password`, `firstname`, `lastname`, `birthday`, `sex`) VALUES
-(1, 'Mickey', 'Mouse', '', '', NULL, ''),
-(2, 'Cheese', 'Hotdog', '', '', NULL, ''),
-(3, 'Foot', 'Long', '', '', NULL, ''),
-(12, 'Rey', 'pass', 'Rey', 'Malicse', '2002-03-03', 'male'),
-(14, 'anakniluka', 'mavs', 'dj', 'aquino', '2003-09-27', 'male'),
-(15, 'reyw', 'pass', 'willarddd', 'malicse', '2002-03-03', 'Select Sex'),
-(16, 'john', 'doe', 'John', 'Doe', '2032-03-03', 'Select Sex'),
-(17, 'jane', 'doe', 'jane', 'Doe', '2332-01-01', 'Female'),
-(18, 'update Uname', 'password', 'firstname', 'lastname', '1000-01-01', 'Select Sex'),
-(20, 'roze', 'roze', 'jei', 'roze', '1002-01-01', 'Select Sex');
+INSERT INTO `users` (`User_Id`, `username`, `password`, `firstname`, `lastname`, `birthday`, `sex`, `user_email`, `user_profile_picture`, `account_type`) VALUES
+(3, 'Jemy', 'memes', '', '', NULL, '', '', '', 0),
+(14, 'anakniluka', 'mavs', 'dj', 'aquino', '2003-09-27', 'male', '', '', 0),
+(22, 'jemjem', 'Jemjem1!', 'jem', 'jem', '1233-03-12', 'Male', 'jeremiahlansang1010@gmail.com', 'uploads/OIP_1716342870.jpg', 0),
+(23, 'kim', '$2y$10$k4Tdz9BGp4RoPfKInSq0AeUfMQxeavO.YfoRN13ldibnKFDyyw6N2', 'kim', 'kim', '1994-02-06', 'Male', 'kimkim@gmail.com', 'uploads/kim.jpg', 0),
+(25, 'akoto', 'akoto', 'akoto', 'ikawto', '2020-11-30', 'male', '', '', 0),
+(26, 'yes', '$2y$10$hTD/UFnvhzyRmUigmRS1eOxhPBY/r9ibxFRdj3FcEgAOntyT79n1y', 'yes', 'yes', '2017-03-05', 'Male', 'yes@gmail.com', 'uploads/3333.jpg', 1),
+(27, 'shes', '$2y$10$Y1i7nhS9BuJlHPIM4GUJD.lm3ZPB6HChrB3UZcPnecv8K1VrON/LG', 'shes', 'awts', '2012-12-01', 'Female', 'shes@gmail.com', 'uploads/121312.jpg', 0);
 
 -- --------------------------------------------------------
 
@@ -73,11 +73,10 @@ CREATE TABLE `user_address` (
 --
 
 INSERT INTO `user_address` (`user_add_id`, `User_Id`, `street`, `barangay`, `city`, `province`) VALUES
-(1, 15, 'sico', 'sico', 'lipa', 'batangas'),
-(2, 16, 'new street', 'violeta', 'New York', 'USA'),
-(3, 17, 'street', 'sico', 'New York', 'USA'),
-(4, 18, 'updated street', 'barangay', 'city', 'province'),
-(6, 20, 'street', 'roze', 'roze', 'roze');
+(8, 22, '132', 'Mangas I', 'Alfonso', 'Region IV-A (CALABARZON)'),
+(9, 23, 'death', 'Igsoro', 'Bugasong', 'Region VI (Western Visayas)'),
+(11, 26, 'wala12', 'Mangino', 'City Of Gapan', 'Region III (Central Luzon)'),
+(12, 27, 'LipaS', 'Malamig', 'Gloria', 'MIMAROPA');
 
 --
 -- Indexes for dumped tables
@@ -104,13 +103,13 @@ ALTER TABLE `user_address`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `User_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `User_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `user_address`
 --
 ALTER TABLE `user_address`
-  MODIFY `user_add_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_add_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
